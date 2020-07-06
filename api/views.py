71c8e5383c4c113a880cea03b548d03e9d284c00
Evgenii Shumilov <mechnotech@yandex.ru>
2020-07-06 15:24:54 +0300

    second try - 0.1.2

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets, permissions
+from rest_framework import permissions, status, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
